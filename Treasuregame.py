775d8553f5a1b903cee0b2209010117bc50814fc
Anshikashukla10 <112242397+Anshikashukla10@users.noreply.github.com>
2023-05-31 15:46:03 +0530

    Add files via upload
    Treasure Island Game in Python

---
+++ Treasuregame.py
@@ -0,0 +1,55 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+stage_1=input("Choose left or Right")
+if stage_1=="left":
+  print("Continue to next step")
+else:
+  print("Oops Game Over!You fell into Hole")
+
+stage_2=input("Do you want to swim or wait? ")
+if stage_2=="wait":
+  print("continue game")
+else:
+  print("Attacked by trout..Game Over!!")
+
+stage_3=input("which door? Red Blue or Yellow?")
+if stage_3=="Red":
+  print("Oops!! Burned by fire..Game Over!!")
+elif stage_3=="Blue":
+  print("Eaten by Beasts..Game Over!!")
+elif stage_3=="Yellow":
+  print("Yayy..Congratulations You Won!!")
+else:
+  print("Game Over!!")
+  
+
+
+
+
+
